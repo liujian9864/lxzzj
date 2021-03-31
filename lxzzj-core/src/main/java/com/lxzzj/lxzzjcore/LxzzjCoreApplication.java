25c66913abcb08b7cd094381a9152656177c7bc9
liujian <1149606140@qq.com>
2021-03-31 18:13:08 +0800

    task

---
+++ lxzzj-core/src/main/java/com/lxzzj/lxzzjcore/LxzzjCoreApplication.java
@@ -0,0 +1,13 @@
+package com.lxzzj.lxzzjcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxzzjCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxzzjCoreApplication.class, args);
+    }
+
+}
